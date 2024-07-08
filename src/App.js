4b5ab25498eb5906939574782a6d4eb341b66953
Nikola Simunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2024-07-08 13:12:56 +0200

    Update App.js
    - use react hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./index.css";
+import { isVisible } from "@testing-library/user-event/dist/utils";
 
 const messages = [
   "Learn React ⚛️",
@@ -9,6 +10,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isVisible, setVisibility] = useState(true);
   //let step = 1;
 
   function handleNext() {
@@ -27,36 +29,48 @@ export default function App() {
     }
   }
 
+  function handleVisibility() {
+    setVisibility(!isVisible);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        {/* case when we use variable instead of state
+    <>
+      <button className="close" onClick={handleVisibility}>
+        {/* Good to use if we have very simple logic <button className="close" onClick={() => setVisibility(!isVisible)}> */}
+        &times;
+      </button>
+      {isVisible && (
+        <div className="steps">
+          <div className="numbers">
+            {/* case when we use variable instead of state
         <div className={`${step === 1 ? "active" : ""}`}>1</div>
         <div className={`${step === 2 ? "active" : ""}`}>2</div>
         <div className={`${step === 3 ? "active" : ""}`}>3</div> */}
 
-        <div className={step === 1 ? "active" : ""}>1</div>
-        <div className={step === 2 ? "active" : ""}>2</div>
-        <div className={step === 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
